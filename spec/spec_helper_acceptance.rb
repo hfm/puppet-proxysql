73761fa9545749ea6cf0361a74911a6224dd0489
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-05-28 02:01:19 +0900

    copy from https://github.com/puppetlabs/beaker-rspec#create-the-spec_helper_acceptancerb

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,24 @@
+require 'beaker-rspec'
+
+logger.error("LOADED MYYYYYYYYYY Spec Acceptance Helper")
+
+# Install Puppet on all hosts
+install_puppet_on(hosts, options)
+
+RSpec.configure do |c|
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  c.formatter = :documentation
+
+  c.before :suite do
+    # Install module to all hosts
+    hosts.each do |host|
+      install_dev_puppet_module_on(host, :source => module_root, :module_name => 'mysql',
+          :target_module_path => '/etc/puppet/modules')
+      # Install dependencies
+      on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
+
+      # Add more setup code as needed
+    end
+  end
+end
