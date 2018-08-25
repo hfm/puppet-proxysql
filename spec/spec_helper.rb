92109648c31ccd2c31fec8c2be667ee4e75a8b85
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-08-25 21:29:15 +0900

    Update to pdk 1.6.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,8 +34,13 @@ RSpec.configure do |c|
     # by default Puppet runs at warning level
     Puppet.settings[:strict] = :warning
   end
+end
 
-  c.after :suite do
-    RSpec::Puppet::Coverage.report!
+def ensure_module_defined(module_name)
+  module_name.split('::').reduce(Object) do |last_module, next_module|
+    last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module)
+    last_module.const_get(next_module)
   end
 end
+
+# 'spec_overrides' from sync.yml will appear below this line
